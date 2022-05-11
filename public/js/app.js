f1b048a8b69a6a09614ad25fc718f8ef8144a786
shivam <shivamreso512@gmail.com>
2022-05-11 19:15:48 +0530

    remove console.log which was unnessacary

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("static js");
-
 const weatherForm = document.querySelector("form");
 const messageOne = document.querySelector("#message-1");
 const messageTwo = document.querySelector("#message-2");
